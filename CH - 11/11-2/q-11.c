a5438fc8494ed68c2eae2de9aaad4626c87c7760
Rajdeep5270 <rajdeepex5270@gmail.com>
2025-05-31 03:12:24 +0530

    Custom Pattern

---
+++ CH - 11/11-2/q-11.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main() {
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int s = 5; s > i; s--)
+        {
+            printf(" ");
+        }
+        
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        
+        for (int j = i - 1; j >= 1; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}
